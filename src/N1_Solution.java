2702d812155c34bc53ddea8bd26f086ec1f8068b
wenjin1997 <wenjin1997@126.com>
2022-01-16 22:26:28 +0800

    更新 1.两数之和

--- src/N1_Solution.java
+++ src/N1_Solution.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 /**
  * @author 文进
  * @version 1.0
@@ -16,6 +18,26 @@ class N1_Solution1 {
             }
         }
         // 未找到
+        return new int[] {-1, -1};
+    }
+}
+
+/** 哈希表 **/
+class N1_Solution2 {
+    public int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+        HashMap<Integer, Integer> index = new HashMap<>();
+        // 构造一个哈希表：元素映射到相应的索引
+        for (int i = 0; i < n; i++)
+            index.put(nums[i], i);
+
+        for (int i = 0; i < n; i++) {
+            int other = target - nums[i];
+            // 如果 other 存在且不是 nums[i] 本身
+            if (index.containsKey(other) && index.get(other) != i)
+                return new int[] {i, index.get(other)};
+        }
+
         return new int[] {-1, -1};
     }
 }
