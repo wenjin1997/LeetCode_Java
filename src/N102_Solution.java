f773cfbcdb0d9d6fcf6d7d7e30026d2bbf38b87e
wenjin1997 <wenjin1997@126.com>
2022-01-30 20:56:08 +0800

    102.二叉树的层序遍历

---
+++ src/N102_Solution.java
@@ -0,0 +1,37 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @author 文进
+ * @version 1.0
+ * 102.二叉树的层序遍历
+ */
+public class N102_Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new LinkedList<>();
+        if (root == null) {
+            return res;
+        }
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        // while 循环控制从上向下一层层遍历
+        while (!q.isEmpty()) {
+            int sz = q.size();
+            // 记录这一层的节点值
+            List<Integer> level = new LinkedList<>();
+            // for 循环控制每一层从左向右遍历
+            for (int i = 0; i < sz; i++) {
+                TreeNode cur = q.poll();
+                level.add(cur.val);
+                if (cur.left != null)
+                    q.offer(cur.left);
+                if (cur.right != null)
+                    q.offer(cur.right);
+            }
+            res.add(level);
+        }
+        return res;
+    }
+}
