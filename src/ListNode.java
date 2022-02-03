cadbb2d0deebcf710d6ef6bf029d2e9603ecd631
wenjin1997 <wenjin1997@126.com>
2022-02-03 22:15:41 +0800

    92.反转链表II

---
+++ src/ListNode.java
@@ -0,0 +1,21 @@
+/**
+ * @author 文进
+ * @version 1.0
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+}
