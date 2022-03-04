3949e6f35038b536954f09f36cab3daf4a4b9408
wenjin1997 <wenjin1997@126.com>
2022-03-04 18:58:22 +0800

    122.买卖股票的最佳时机II

---
+++ src/N122_Solution.java
@@ -0,0 +1,35 @@
+/**
+ * @author 文进
+ * @version 1.0
+ * 122.买卖股票的最佳时机II
+ */
+public class N122_Solution {
+    // 原始版本
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int[][] dp = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            if (i - 1 == -1) {
+                // base case
+                dp[i][0] = 0;
+                dp[i][1] = - prices[i];
+                continue;
+            }
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+        }
+        return dp[n - 1][0];
+    }
+
+    // 空间复杂度优化版本
+    int maxProfit_k_inf(int[] prices) {
+        int n = prices.length;
+        int dp_i_0 = 0, dp_i_1 = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            int temp = dp_i_0;
+            dp_i_0 = Math.max(dp_i_0, dp_i_1 + prices[i]);
+            dp_i_1 = Math.max(dp_i_1, temp - prices[i]);
+        }
+        return dp_i_0;
+    }
+}
