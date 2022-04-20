1f45e1516aa0d0c24f09ef2323d8894cee1d7fc9
wenjin1997 <wenjin1997@126.com>
2022-04-20 11:06:08 +0800

    206.反转链表

---
+++ src/206.反转链表.java
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode.cn id=206 lang=java
+ *
+ * [206] 反转链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    // 递归
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode newHead = reverseList(head.next);
+        head.next.next = head;
+        head.next = null; // 一定不要忘了让 head 的 next 指向 null
+        return newHead;
+    }
+
+    // 迭代
+    public ListNode reverseList2(ListNode head) {
+        ListNode prev = null, cur = head;
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        return prev;
+    }
+}
+// @lc code=end
+
