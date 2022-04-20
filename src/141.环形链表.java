989cba7ca07ac6f45585db8d76982e1346291439
wenjin1997 <wenjin1997@126.com>
2022-04-20 10:10:39 +0800

    141.环形链表

---
+++ src/141.环形链表.java
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=141 lang=java
+ *
+ * [141] 环形链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        // 快慢指针
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) return true;
+        }
+        return false;
+    }
+}
+// @lc code=end
+
